4de8fe4745757e563c092a1b0955ca84ed278a78
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-03 11:46:54 +1000

    Create 53-maximum-subarray.java

---
+++ 53-maximum-subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    /* Greedy algorithm */
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 0) { return 0; }
+        
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            if(currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            
+            if(currentSum <= 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+}
