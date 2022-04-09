5dae56082541639f4854d7c063fc4322de450fc9
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-09 12:15:15 +1000

    Update and rename 18-coin-change-2.java to 518-coin-change-2.java

--- 518-coin-change-2.java
+++ 518-coin-change-2.java
@@ -3,10 +3,9 @@ class Solution {
         int[] dp = new int[amount + 1];
         dp[0] = 1;
         
-        for(int i = 0; i < coins.length; i++) {
-            int coinValue = coins[i];
-            for(int j = coinValue; j <= amount; j++) {
-                dp[j] += dp[j - coinValue];
+        for(int coin : coins) {
+            for(int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
             }
         }
         
