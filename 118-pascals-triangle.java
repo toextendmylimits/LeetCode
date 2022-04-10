84fe0402a830a9314fd682ca35dac6c23b0393c6
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-10 21:08:32 +1000

    Update 118-pascals-triangle.java

--- 118-pascals-triangle.java
+++ 118-pascals-triangle.java
@@ -1,23 +1,23 @@
 class Solution {
-    private static final int PASCAL_EDGE_VALUE = 1;
+    private static final int LEFT_RIGHT_EDGE = 1;
   
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> triangle = new ArrayList<>();
-        triangle.add(Arrays.asList(PASCAL_EDGE_VALUE));
+        triangle.add(Arrays.asList(LEFT_RIGHT_EDGE));
         
         for(int rowIndex = 1; rowIndex < numRows; rowIndex++) {
             List<Integer> row = new ArrayList<>();
             List<Integer> previousRow = triangle.get(rowIndex - 1);
             
             // First column is 1
-            row.add(PASCAL_EDGE_VALUE);
+            row.add(LEFT_RIGHT_EDGE);
             
             for(int columnIndex = 1; columnIndex <= rowIndex - 1; columnIndex++) {
                 row.add(previousRow.get(columnIndex) + previousRow.get(columnIndex - 1));
             }
             
             // Last column is 1
-            row.add(PASCAL_EDGE_VALUE);
+            row.add(LEFT_RIGHT_EDGE);
             
             triangle.add(row);
         }
