788d37272c65dd644df0af1b8cbaaaeabc765546
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-10 20:06:23 +1000

    Update 50-powx-n.java

--- 50-powx-n.java
+++ 50-powx-n.java
@@ -1,13 +1,15 @@
+// Solution 1 recurision, solution 2 iteration
 class Solution {
     public double myPow(double x, int n) {
-        if(n < 0) {
-            return calculatePositivePow(1/x, -n);
+        long longValue = n;
+        if(longValue < 0) {
+            return calculatePositivePow(1/x, -longValue);
         }
         
-        return calculatePositivePow(x, n);
+        return calculatePositivePow(x, longValue);
     }
     
-    private double calculatePositivePow(double x, int n) {
+    private double calculatePositivePow(double x, long n) {
         if(n == 0) { return 1.0; }
 
         double halfPow = calculatePositivePow(x, n / 2);
@@ -15,4 +17,16 @@ class Solution {
         
         return n % 2 == 0? squareOfHalfPow : squareOfHalfPow * x;
     }
+    
+    /*private double calculatePositivePow(double x, long n) {
+        double result = 1.0;
+        for(long i = n; i > 0; i /= 2) {
+            if(i % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+        }
+        
+        return result;
+    }*/    
 }
