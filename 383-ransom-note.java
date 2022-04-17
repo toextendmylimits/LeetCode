96c8a9fd58a1ca49dd95b16b6e5e8644f65fc858
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-17 13:24:35 +1000

    Update 383-ransom-note.java

--- 383-ransom-note.java
+++ 383-ransom-note.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static final int LOWER_CASE_LETTERS_COUNT = 26;
+    /*public static final int LOWER_CASE_LETTERS_COUNT = 26;
     public static final char SMALLEST_LOWER_CASE_LETTER = 'a';
     
     public boolean canConstruct(String ransomNote, String magazine) {
@@ -25,5 +25,34 @@ class Solution {
     
     private int getLowercaseLetterIndex(char c) {
         return c - SMALLEST_LOWER_CASE_LETTER;
+    }*/
+    
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        
+        Map<Character, Integer> charFrequencyMap = getCharFrequencyMap(magazine);
+        
+        for(int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            int frequency = charFrequencyMap.getOrDefault(c, 0);
+            if(frequency == 0) {
+                return false;
+            }
+            charFrequencyMap.put(c, frequency - 1);
+        }
+        
+        return true;
+    } 
+    
+    private Map<Character, Integer> getCharFrequencyMap(String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < magazine.length(); i++) {
+            char c = magazine.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        return map;
     }
 }
