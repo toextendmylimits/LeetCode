d621c56b866b40551ec6350c887d3876f6276aee
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-17 23:19:32 +1000

    Create 680-valid-palindrome-ii.java

---
+++ 680-valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left <= right) {
+            if(s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            }
+            else {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean isPalindrome(String s, int start, int end) {
+        for(int left = start, right = end; left <= right; left++, right--) {
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
