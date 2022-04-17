ae1becf9a0f28fbd231beb91fdb2b5cfd0b26791
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-17 20:03:06 +1000

    Create 766-toeplitz-matrix.java

---
+++ 766-toeplitz-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    /*public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int row = matrix.length - 1; row >= 1; row--) {
+            int[] cellsInRow = matrix[row];
+            for(int column = cellsInRow.length - 1; column >= 1; column--) {
+                if(cellsInRow[column] != matrix[row - 1] [column - 1]) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }*/
+    
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int row = 1; row < matrix.length; row++) {
+            for(int column = 1; column < matrix[0].length; column++) {
+                if(matrix[row][column] != matrix[row - 1] [column - 1]) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }    
+}
