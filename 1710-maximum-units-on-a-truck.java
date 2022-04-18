9e68c34285685a9bb786d4da5470904062521428
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-18 23:22:57 +1000

    Update 1710-maximum-units-on-a-truck.java

--- 1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck.java
@@ -3,11 +3,11 @@ class Solution {
         Arrays.sort(boxTypes, (a, b) -> b[1] - a [1]);
         
         int totalUnit = 0;
-        for(int i = 0; i < boxTypes.length; i++) {
+        for(int[] boxType : boxTypes) {
             if(truckSize > 0) {
-                int boxesToPutOnTruck = Math.min(boxTypes[i][0], truckSize);
+                int boxesToPutOnTruck = Math.min(boxType[0], truckSize);
                 truckSize -= boxesToPutOnTruck;
-                totalUnit += boxesToPutOnTruck * boxTypes[i][1];
+                totalUnit += boxesToPutOnTruck * boxType[1];
             }
             else {
                 break;
