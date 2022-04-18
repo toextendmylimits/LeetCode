9ee874e111888400ba52b82645c0b4530c240ebd
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-18 18:18:43 +1000

    Update 937-reorder-data-in-log-files.java

--- 937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files.java
@@ -1,5 +1,38 @@
 class Solution {
-    public String[] reorderLogFiles(String[] logs) {
+     public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, (a, b) -> {
+            String[] partsA = a.split(" ", 2);
+            String[] partsB = b.split(" ", 2);
+            String idA = partsA[0];
+            String contentA = partsA[1];
+            String idB = partsB[0];
+            String contentB = partsB[1];
+            boolean isDigitA = Character.isDigit(contentA.charAt(0));
+            boolean isDigitB = Character.isDigit(contentB.charAt(0));
+            if(isDigitA && isDigitB) {
+                return 0;
+            }
+            else if(!isDigitA && isDigitB) {
+                return -1;
+            }
+            else if(isDigitA && !isDigitB) {
+                return 1;
+            }
+            else {
+                int compareContentResult = contentA.compareTo(contentB);
+                if(compareContentResult == 0) {
+                    return idA.compareTo(idB);
+                }
+                else {
+                    return compareContentResult;
+                }
+            }
+        });
+         
+         return logs;
+    }
+    
+    /*public String[] reorderLogFiles(String[] logs) {
         List<String> letterLogs = new ArrayList<>();
         List<String> digitLogs = new ArrayList<>();
         
@@ -65,5 +98,5 @@ class Solution {
     
     private int getFirstSpaceIndex(String log) {
         return log.indexOf(" ");
-    }
+    }*/
 }
