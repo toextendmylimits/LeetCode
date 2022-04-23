d24ad9717704e8dc0f098b2bd0a3c24b33c3578b
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-24 08:50:35 +1000

    Create 227-basic-calculator-ii.java

---
+++ 227-basic-calculator-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        char operator = '+';
+        int currentNum = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if(Character.isDigit(currentChar)) {
+                currentNum = currentNum * 10 + (currentChar - '0');
+            }
+            
+            if(!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == s.length() - 1) {
+                if(operator == '+') {
+                    stack.push(currentNum);
+                }
+                else if(operator == '-') {
+                    stack.push(-currentNum);
+                }
+                else if(operator == '*') {
+                    stack.push(stack.pop() * currentNum);
+                }
+                else if(operator == '/') {
+                    stack.push(stack.pop() / currentNum);
+                }
+                
+                operator = currentChar;
+                currentNum = 0;
+            }
+        }
+        
+        int result = 0;
+        while(!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        
+        return result;
+    }
+}
