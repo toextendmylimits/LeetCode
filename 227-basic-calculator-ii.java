6b5e8fd6b3dd0927f975404924bbe5dde6a75ad4
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-24 10:18:53 +1000

    Update 227-basic-calculator-ii.java

--- 227-basic-calculator-ii.java
+++ 227-basic-calculator-ii.java
@@ -1,5 +1,43 @@
 class Solution {
     public int calculate(String s) {
+        char operator = '+';
+        int currentNum = 0;
+        int result = 0;
+        int lastNum = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if(Character.isDigit(currentChar)) {
+                currentNum = currentNum * 10 + (currentChar - '0');
+            }
+            
+            if(!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == s.length() - 1) {
+                if(operator == '+') {
+                    result += lastNum;
+                    lastNum = currentNum;
+                }
+                else if(operator == '-') {
+                    result += lastNum;
+                    lastNum = - currentNum;
+                }
+                else if(operator == '*') {
+                    lastNum = lastNum * currentNum;
+                }
+                else if(operator == '/') {
+                    lastNum = lastNum / currentNum;
+                }
+                
+                operator = currentChar;
+                currentNum = 0;
+            }
+        }
+        
+        result += lastNum;
+        
+        return result;
+    }
+    
+    /*public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
         char operator = '+';
         int currentNum = 0;
@@ -35,5 +73,5 @@ class Solution {
         }
         
         return result;
-    }
+    }*/
 }
