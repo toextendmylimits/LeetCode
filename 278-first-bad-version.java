cdfbfe1886f9e3a0713179bcd62010bb5f6b6b43
toextendmylimits <bruce.yang.it@gmail.com>
2022-04-25 18:02:08 +1000

    Update 278-first-bad-version.java

--- 278-first-bad-version.java
+++ 278-first-bad-version.java
@@ -22,4 +22,21 @@ public class Solution extends VersionControl {
         
         return result;
     }
+     // Different pattern left < right 
+    /*public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while(left < right) {
+            int middle = left + (right - left) / 2;
+            if(isBadVersion(middle)) {
+                right = middle;
+            }
+            else {
+                left = middle + 1;
+            }
+        }
+        
+        return left;
+    }*/     
 }
