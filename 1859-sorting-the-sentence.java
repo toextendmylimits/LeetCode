2c759cbc66ab9ebfec63885612fa1547912fc652
toextendmylimits <bruce.yang.it@gmail.com>
2022-05-03 19:29:05 +1000

    Create 1859-sorting-the-sentence.java

---
+++ 1859-sorting-the-sentence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] newWords = new String[words.length];
+        for(String word : words) {
+            int pos = word.charAt(word.length() - 1) - '0' - 1;
+            newWords[pos] = word.substring(0, word.length() - 1);
+        }
+        
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < newWords.length - 1; i++) {
+            result.append(newWords[i]);
+            result.append(" ");
+        }
+        result.append(newWords[newWords.length - 1]);
+        
+        return result.toString();
+    }
+}
