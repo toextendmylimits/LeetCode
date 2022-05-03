8d044a38b00823bc47f2e005bf9b245c606a49b3
toextendmylimits <bruce.yang.it@gmail.com>
2022-05-03 19:32:20 +1000

    Update 1859-sorting-the-sentence.java

--- 1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence.java
@@ -7,13 +7,15 @@ class Solution {
             newWords[pos] = word.substring(0, word.length() - 1);
         }
         
-        StringBuilder result = new StringBuilder();
+        /*StringBuilder result = new StringBuilder();
         for(int i = 0; i < newWords.length - 1; i++) {
             result.append(newWords[i]);
             result.append(" ");
         }
         result.append(newWords[newWords.length - 1]);
         
-        return result.toString();
+        return result.toString();*/
+        
+        return String.join(" ", newWords);
     }
 }
