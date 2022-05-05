482c61038ed552c04f5aa18a0882e5ddd5dec2f1
toextendmylimits <bruce.yang.it@gmail.com>
2022-05-05 16:13:48 +1000

    Update 249-group-shifted-strings.java

--- 249-group-shifted-strings.java
+++ 249-group-shifted-strings.java
@@ -4,30 +4,30 @@ class Solution {
         for(String s : strings) {
             if(s.length() == 0) { continue; }
             
-            int offset = s.charAt(0) - 'a';
-            StringBuilder keySb = new StringBuilder();
-            keySb.append('a');
-            
-            for(int i = 1; i < s.length(); i++) {
-                char ch = (char)(s.charAt(i) - offset);
-                if(ch < 'a') {
-                    ch = (char)(ch + 26);
-                }
-                keySb.append(ch);
-            }
-            
-            String key = keySb.toString();
+            String key = shiftToStringStartWithA(s);
             if(!map.containsKey(key)) {
                 map.put(key, new ArrayList<>());
             }
             map.get(key).add(s);
         }
         
-        List<List<String>> result = new ArrayList<>();
-        for(List<String> list : map.values()) {
-            result.add(list);
+        return new ArrayList<>(map.values());
+    }
+    
+    private String shiftToStringStartWithA(String s) {
+        StringBuilder result = new StringBuilder();
+        
+        int offset = s.charAt(0) - 'a';
+        result.append('a');
+        
+         for(int i = 1; i < s.length(); i++) {
+            char ch = (char)(s.charAt(i) - offset);
+            if(ch < 'a') {
+                ch = (char)(ch + 26);
+            }
+            result.append(ch);
         }
         
-        return result;
+        return result.toString();
     }
 }
