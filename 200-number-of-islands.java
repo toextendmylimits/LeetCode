36247f15ce39da1e91954b4cea1a252f7f939617
toextendmylimits <bruce.yang.it@gmail.com>
2022-05-07 23:22:19 +1000

    Update 200-number-of-islands.java

--- 200-number-of-islands.java
+++ 200-number-of-islands.java
@@ -4,6 +4,52 @@ class Solution {
         int n = grid[0].length;
         
         int numOfIslands = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1') {
+                    numOfIslands++;
+                    grid[i][j] = '0';
+                    
+                    Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+                    queue.add(new Pair<>(i, j));
+                    while(!queue.isEmpty()) {
+                        Pair<Integer, Integer> temp = queue.poll();
+                        int row = temp.getKey();
+                        int col = temp.getValue();
+                        
+                        if(row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            queue.offer(new Pair<>(row - 1, col));
+                            grid[row - 1][col] = '0';
+                        }
+                        
+                        if(row + 1 < m && grid[row + 1][col] == '1') {
+                            queue.offer(new Pair<>(row + 1, col));
+                            grid[row + 1][col] = '0'; 
+                        }
+                        
+                        if(col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            queue.offer(new Pair<>(row, col - 1));
+                            grid[row][col - 1] = '0';
+                        }
+                        
+                        if(col + 1 < n && grid[row][col + 1] == '1') {
+                            queue.offer(new Pair<>(row, col + 1));
+                            grid[row][col + 1] = '0'; 
+                        }                        
+                    }
+                }
+            }
+        }
+        
+        return numOfIslands;
+    }
+    
+    /*public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        int numOfIslands = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] == '1') {
@@ -14,7 +60,7 @@ class Solution {
         }
         
         return numOfIslands;
-    }
+    }*/
     
     private void dfs(char[][] grid, int i, int j) {
         int m = grid.length;
