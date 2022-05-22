146d188c31dba85831649956d44fc56183eee1da
toextendmylimits <bruce.yang.it@gmail.com>
2022-05-22 21:04:17 +1000

    Update 98-validate-binary-search-tree.java

--- 98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree.java
@@ -16,16 +16,54 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         return validate(root, null, null);
-    }
+    }  
     
-    // Recursive Traversal with Valid Range
     private boolean validate(TreeNode root, Integer low, Integer high) {
         if(root == null) { return true; }
+        if((low != null && root.val <= low) || (high != null && root.val >= high)) { return false; }
         
-        if((low != null && root.val <= low) || (high != null && root.val >= high)) {
+        return validate(root.left, low, root.val) && validate(root.right, root.val, high);
+    }
+    /*public boolean isValidBST(TreeNode root) {
+        if(root == null) { return true; }
+        
+        Integer prev = null;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode current = root;
+        while(current != null || !stack.isEmpty()) {
+            if(current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            else {
+                 current = stack.pop();
+                if(prev != null && current.val <= prev) {
+                    return false;
+                }
+                prev = current.val;
+                current = current.right;               
+            }
+        }
+        
+        return true;
+    }*/
+    
+    Integer prev;
+    /*public boolean isValidBST(TreeNode root) {
+        prev = null;
+        return inorder(root);
+    }*/
+    
+    private boolean inorder(TreeNode root) {
+        if(root == null) { return true; }
+        
+        if(!inorder(root.left)) { return false; }
+        
+        if(prev != null && root.val <= prev) {
             return false;
         }
         
-        return validate(root.left, low, root.val) && validate(root.right, root.val, high);
+        prev = root.val;
+        return inorder(root.right);
     }
 }
