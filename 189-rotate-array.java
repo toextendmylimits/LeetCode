3f500a4dacb7f977d267b2f4c4c74e4577468ac5
toextendmylimits <bruce.yang.it@gmail.com>
2022-06-04 12:40:52 +1000

    Update 189-rotate-array.java

--- 189-rotate-array.java
+++ 189-rotate-array.java
@@ -12,7 +12,7 @@ class Solution {
         }
     }*/
     
-    public void rotate(int[] nums, int k) {
+    /*public void rotate(int[] nums, int k) {
         int len = nums.length;
         int[] result = new int[len];
       
@@ -23,5 +23,29 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
           nums[i] = result[i];
         }
-    }    
+    }*/ 
+    
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        
+        k %= len;
+        reverse(nums, 0, len - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, len - 1);
+    }  
+    
+    public void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+        /*for(int i = start, j = end; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }*/
+    }
 }
