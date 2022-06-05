df700f99b539213cec30aaca589cd3238348c4e6
toextendmylimits <bruce.yang.it@gmail.com>
2022-06-05 14:44:25 +1000

    Create 204-count-primes.java

---
+++ 204-count-primes.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        boolean[] notPrime = new boolean[n];
+        for(int i = 2; i < n; i++) {
+            if(notPrime[i] == false) {
+                count++;
+                for(int j = i * 2; j < n; j += i) {
+                    notPrime[j] = true;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    /*public int countPrimes(int n) {
+        int count = 0;
+        for(int i = 2; i < n; i++) {
+            if(isPrime(i)) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    public boolean isPrime(int n) {
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }*/
+}
