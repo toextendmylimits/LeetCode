006e3392a87e8b65708a1261ef06412ca76a0ca4
toextendmylimits <bruce.yang.it@gmail.com>
2022-06-13 12:15:04 +1000

    Update 15-3sum.java

--- 15-3sum.java
+++ 15-3sum.java
@@ -1,61 +1,49 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i = 0; i < nums.length && nums[i] <= 0; i++) {
             if(i == 0 || nums[i] != nums[i - 1]) {
-               Set<Integer> seen = new HashSet<>();
-                for(int j = i + 1; j < nums.length; j++) {
-                    int complement = - nums[i] - nums[j];
-                    if(seen.contains(complement)) {
-                        result.add(Arrays.asList(nums[i], nums[j], complement));
-                        while(j < nums.length - 1 && nums[j] == nums[j + 1]) {
-                            j++;
-                        }
-                    }
-                    seen.add(nums[j]);
-                }
-            }
+                twoSum(nums, i, result);
+            }   
         }
         
         return result;
     }
     
-    /*public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
-            if(i == 0 || nums[i] != nums[i - 1]) {
-                int low = i + 1;
-                int high = nums.length - 1;
-                while(low < high) {
-                    int sum = nums[i] + nums[low] + nums[high];
-                    if(sum > 0) {
-                        high--;
-                    }
-                    else if(sum < 0) {
-                        low++;
-                    }
-                    else {
-                        result.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                        
-                        while(low < high && nums[low] == nums[low + 1]) {
-                            low++;
-                        }
-                        
-                        while(low < high && nums[high] == nums[high - 1]) {
-                            high--;
-                        }
-                        
-                        low++;
-                        high--;
-                    }
+    /*public void twoSum(int[] nums, int i, List<List<Integer>> result) {
+        int left = i + 1;
+        int right = nums.length - 1;
+        while(left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+            if(sum < 0) {
+                left++;
+            }
+            else if(sum > 0) {
+                right--;
+            }
+            else {
+                result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                left++;
+                right--;
+                while(left < right && nums[left] == nums[left - 1]) {
+                    left++;
                 }
             }
         }
-        
-        return result;
     }*/
+    public void twoSum(int[] nums, int i, List<List<Integer>> result) {
+        Set<Integer> seen = new HashSet<>();
+        for(int j = i + 1; j < nums.length; j++) {
+            int complement = - nums[i] - nums[j];
+            if(seen.contains(complement)) {
+                result.add(Arrays.asList(nums[i], nums[j], complement));
+                while(j + 1 < nums.length && nums[j] == nums[j + 1]) {
+                    j++;
+                }
+            }
+            
+            seen.add(nums[j]);
+        }
+    }    
 }
