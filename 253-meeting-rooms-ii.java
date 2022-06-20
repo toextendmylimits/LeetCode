d591e0da029542bbfb47edc193bc3094d68fa915
toextendmylimits <bruce.yang.it@gmail.com>
2022-06-20 15:33:56 +1000

    Update 253-meeting-rooms-ii.java

--- 253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii.java
@@ -1,28 +1,42 @@
 class Solution {
-    public int minMeetingRooms(int[][] intervals) {
-        int meetings = intervals.length;
-        int[] starts = new int[meetings];
-        int[] ends = new int[meetings];
-        for(int i = 0; i < meetings; i++) {
-            int[] interval = intervals[i];
-            starts[i] = interval[0];
-            ends[i] = interval[1];
+    /*public int minMeetingRooms(int[][] intervals) {
+        // Sort meeting ascendingly by start time
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);    
+        
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        for(int[] interval : intervals) {
+            heap.offer(interval);
+            if(heap.peek()[1] <= interval[0]) { // Remove a meeting as it finishes
+                heap.poll();
+            }
         }
         
-        Arrays.sort(starts);
-        Arrays.sort(ends);
+        return heap.size();
+    }*/
+    public int minMeetingRooms(int[][] intervals) {
+        // Sort meeting ascendingly by start time
+        int len = intervals.length;
+        int[] startTimes = new int[len] ;
+        int[] endTimes = new int[len];
+        for(int i = 0; i < len; i++) {
+            startTimes[i] = intervals[i][0];
+            endTimes[i] = intervals[i][1];
+        }
+        
+        Arrays.sort(startTimes);
+        Arrays.sort(endTimes);
         
         int rooms = 0;
-        int endIndex = 0;
-        for(int i = 0; i < meetings; i++) {
-            if(starts[i] < ends[endIndex]) {
+        int earlisetEndTimeIndex = 0;
+        for(int i = 0; i < len; i++) {
+            if(startTimes[i] < endTimes[earlisetEndTimeIndex]) {
                 rooms++;
             }
             else {
-                endIndex++;
+                earlisetEndTimeIndex++;
             }
         }
         
         return rooms;
-    }
+    }    
 }
