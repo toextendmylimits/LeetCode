40e1da8b6d9a272ef07b97270d8205ac8bebf0cb
toextendmylimits <bruce.yang.it@gmail.com>
2022-06-23 23:04:18 +1000

    Update 852-peak-index-in-a-mountain-array.java

--- 852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array.java
@@ -24,4 +24,22 @@ class Solution {
         
         return low;
     }  
+    
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 2;
+        int result = -1;
+        while(left <= right) {
+            int middle = left + (right - left) / 2;
+            if(arr[middle] > arr[middle + 1]) {
+                result = middle;
+                right = middle - 1;
+            }
+            else {
+                left = middle + 1;
+            }
+        }
+        
+        return result;
+    }     
 }
