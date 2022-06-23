57300d0b9ae5015f30e2d93ac496ee468883eba0
toextendmylimits <bruce.yang.it@gmail.com>
2022-06-23 23:22:22 +1000

    Update 205-isomorphic-strings.java

--- 205-isomorphic-strings.java
+++ 205-isomorphic-strings.java
@@ -26,4 +26,29 @@ class Solution {
     
        return true;
     }
+    
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) { return false; }
+        
+        Map<Character, Integer> charFirstIndexMapOfS = new HashMap<>();
+        Map<Character, Integer> charFirstIndexMapOfT = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            char charOfS = s.charAt(i);
+            char charOfT = t.charAt(i);
+            if(charFirstIndexMapOfS.containsKey(charOfS) && charFirstIndexMapOfT.containsKey(charOfT)) {
+                if(charFirstIndexMapOfS.get(charOfS) != charFirstIndexMapOfT.get(charOfT)) {
+                    return false;
+                }
+            }
+            else if (!charFirstIndexMapOfS.containsKey(charOfS) && !charFirstIndexMapOfT.containsKey(charOfT)) {
+                charFirstIndexMapOfS.put(charOfS, i);
+                charFirstIndexMapOfT.put(charOfT, i);
+            }
+            else {
+                return false;
+            }
+        }
+                  
+       return true;
+    }    
 }
