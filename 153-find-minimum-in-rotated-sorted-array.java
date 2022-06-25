88dc8ef3f6f2b2e1173c1a4b725772be67a9134d
toextendmylimits <bruce.yang.it@gmail.com>
2022-06-25 17:54:41 +1000

    Update 153-find-minimum-in-rotated-sorted-array.java

--- 153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array.java
@@ -15,4 +15,22 @@ class Solution {
         
         return nums[left];
     }
+    
+    public int findMin(int[] nums) {
+        int result = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] <= nums[nums.length - 1]) {
+                result = nums[mid];
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return result;
+    }    
 }
