6cec53168d1a01e79f34f9be925f50dacca1fb80
toextendmylimits <bruce.yang.it@gmail.com>
2022-07-23 19:57:05 +1000

    Update 15-3sum.java

--- 15-3sum.java
+++ 15-3sum.java
@@ -1,5 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(i == 0 || nums[i] != nums[i - 1]) {
+                int targetSum = -nums[i];
+                int left = i + 1;
+                int right = nums.length - 1;
+                while(left < right) {
+                    int currTwoSum = nums[left] + nums[right];
+                    if(currTwoSum > targetSum) {
+                        right--;
+                    }
+                    else if(currTwoSum < targetSum) {
+                        left++;
+                    }
+                    else {
+                        result.add(List.of(nums[i], nums[left], nums[right]));
+                        left++;
+                        right--;
+                        while(left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         for(int i = 0; i < nums.length && nums[i] <= 0; i++) {
