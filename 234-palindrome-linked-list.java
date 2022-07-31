c743c2942ce9da310986adf69032246cdefce798
toextendmylimits <bruce.yang.it@gmail.com>
2022-07-31 22:19:52 +1000

    Update 234-palindrome-linked-list.java

--- 234-palindrome-linked-list.java
+++ 234-palindrome-linked-list.java
@@ -1,110 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-     public boolean isPalindrome(ListNode head) {       
-        List<ListNode> nodes = new ArrayList<>();
-         ListNode curr = head;
-         while(curr != null) {
-             nodes.add(curr);
-             curr = curr.next;
-         }
-         
-         for(int i = 0, j = nodes.size() - 1; i < j; i++, j--) {
-             if(nodes.get(i).val != nodes.get(j).val) {
-                 return false;
-             }
-         }
-         
-         return true;
-    }   
     /*public boolean isPalindrome(ListNode head) {
-        if(head == null) { return true; }
-        
-        ListNode firstHalfEnd = getFirstHalfEnd(head);
-        ListNode secondHalfStart = reverse(firstHalfEnd.next);
-        
-        boolean result = true;
-        ListNode p1 = head;
-        ListNode p2 = secondHalfStart;
-        while(result && p2 != null) {
-            if(p1.val != p2.val) { result = false; }
-            
-            p1 = p1.next;
-            p2 = p2.next;
+        List<ListNode> nodes = new ArrayList<>();
+        while(head != null) {
+            nodes.add(head);
+            head = head.next;
         }
         
-        firstHalfEnd.next = reverse(secondHalfStart);
-        return result;
-    }*/
-    
-    private ListNode getFirstHalfEnd(ListNode head) {
-       ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        
-        return slow;
-    }
-    
-    private ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        while(curr != null) {
-            ListNode nextTemp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextTemp;
-        }
-        
-        return prev;
-    }
-     
-    public boolean isPalindrome(ListNode head) {
-        // Find middle
-        ListNode middle = getMiddle(head);
-        
-        // Reverse second half
-        ListNode tail = reverse(middle);
-
-        // Compare seconcd half with first half
-        boolean isPalindrome = isEqual(tail, head);
-        
-        // Resotre second half
-        reverse(tail);
-        
-        return isPalindrome;
-    }
-    
-    private ListNode getMiddle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        while(fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        
-        return slow;
-    }
-    
-    private boolean isEqual(ListNode shorterHalf, ListNode longerHalf) {
-        while(shorterHalf != null) {
-            if(shorterHalf.val != longerHalf.val) {
+        for(int start = 0, end = nodes.size() - 1; start < end; start++, end--) {
+            if(nodes.get(start).val != nodes.get(end).val) {
                 return false;
             }
-            shorterHalf = shorterHalf.next;
-            longerHalf = longerHalf.next;
         }
+        
+        return true;
+    }*/
+    
+    public boolean isPalindrome(ListNode head) {
+        if(head == null) {
+            return true;
+        }
+               
+        ListNode endOfFirstHalf = geEndOfFirstHalf(head);
+        ListNode startOfSecondHalf = endOfFirstHalf.next;
+            
+        boolean isPalindrome = arePalindromeHalfs(head, reverse(startOfSecondHalf));       
+        endOfFirstHalf.next = reverse(startOfSecondHalf);
+        
+        return isPalindrome;
+    }  
+    
+    private ListNode geEndOfFirstHalf(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        return slow;
+    }
+    
+    private boolean arePalindromeHalfs(ListNode firstHalf, ListNode secondHalf) {
+        while(secondHalf != null) {
+            if(firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            secondHalf = secondHalf.next;
+            firstHalf = firstHalf.next;
+        }
+        
         return true;
     }
     
-    private ListNode reverse(ListNode head) {
+    public ListNode reverse(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
         while(curr != null) {
-            ListNode next = curr.next;
+            ListNode oldNext = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = next;
+            curr = oldNext;
         }
         
         return prev;
