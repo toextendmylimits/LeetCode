b3dba1359267c3fd1cdfe14ae8c8a9ac2d06c8b0
toextendmylimits <bruce.yang.it@gmail.com>
2022-08-01 15:57:00 +1000

    Update 25-reverse-nodes-in-k-group.java

--- 25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group.java
@@ -1,3 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         ListNode dummy = new ListNode(-1);
@@ -36,8 +46,7 @@ class Solution {
         ListNode tail = preFirstNode.next;
         tail.next = curr;
         preFirstNode.next = prev;
-        preFirstNode = tail;
         
-        return preFirstNode;
+        return tail;
     }
 }
