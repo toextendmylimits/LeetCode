a84e96d0e2322114d0518a8cd5796aeeb70dc0d3
toextendmylimits <bruce.yang.it@gmail.com>
2022-08-01 15:45:37 +1000

    Create 25-reverse-nodes-in-k-group.java

---
+++ 25-reverse-nodes-in-k-group.java
@@ -0,0 +1,43 @@
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        ListNode preFirstNodeOfKGroup = dummy;
+        while(preFirstNodeOfKGroup != null && preFirstNodeOfKGroup.next != null) {
+            ListNode node = preFirstNodeOfKGroup;
+            int i = 0;
+            while(i < k && node.next != null) {
+                node = node.next;
+                i++;
+            }
+            
+            // Less than k nodes left
+            if(i < k) {
+                break;
+            }
+            
+            preFirstNodeOfKGroup = getPreFirstNodeAfterReverse(preFirstNodeOfKGroup, k);
+        }
+        
+        return dummy.next;
+    }
+    
+    private ListNode getPreFirstNodeAfterReverse(ListNode preFirstNode, int k) {
+        ListNode prev = null;
+        ListNode curr = preFirstNode.next;
+        for(int i = 0; i < k; i++) {
+            ListNode oldNext = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = oldNext;
+        }
+
+        ListNode tail = preFirstNode.next;
+        tail.next = curr;
+        preFirstNode.next = prev;
+        preFirstNode = tail;
+        
+        return preFirstNode;
+    }
+}
