767e1d0309a8f76f815d67878131e5049a361815
toextendmylimits <bruce.yang.it@gmail.com>
2022-08-04 21:48:23 +1000

    Update 844-backspace-string-compare.java

--- 844-backspace-string-compare.java
+++ 844-backspace-string-compare.java
@@ -1,64 +1,45 @@
 class Solution {
-     public boolean backspaceCompare(String s, String t) {
-        int p1 = s.length() - 1;
-         int p2 = t.length() - 1;
-         int backspaces1 = 0; // Represent remaining number of backspace operations for s
-         int backspaces2 = 0; // Represent remaining number of backspace operations for t
-         while(p1 >= 0 || p2 >= 0) {
-             while(p1 >= 0 &&(s.charAt(p1) == '#' || backspaces1 != 0)) {
-                 if(s.charAt(p1) == '#') { backspaces1++; }
-                 else { backspaces1--; }
-                 p1--;
-             }
-             
-             while(p2 >= 0 &&(t.charAt(p2) == '#' || backspaces2 != 0)) {
-                 if(t.charAt(p2) == '#') { backspaces2++; }
-                 else { backspaces2--; }
-                 p2--;
-             } 
-             
-             if(p1 < 0 && p2 < 0) { return true; }
-             if(p1 < 0 || p2 < 0) { return false; }
-             if(s.charAt(p1) == t.charAt(p2)) {
-                 p1--;
-                 p2--;
-             }
-             else {
-                 return false;
-             }
-         }
-         
-         return true;
-    }   
-    /*public boolean backspaceCompare(String s, String t) {
-        return backspace(s).equals(backspace(t));    
-    }*/
-    
-    /*private String backspace(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
-        for(char c : s.toCharArray()) {
-            if(c != '#') {
-                stack.push(c);
+    public boolean backspaceCompare(String s, String t) {
+        int index1 = s.length() - 1;
+        int index2 = t.length() - 1;
+        while(index1 >= 0 || index2 >= 0) {
+            int nextIndex1 = getNextValidCharIndex(s, index1);
+            int nextIndex2 = getNextValidCharIndex(t, index2);
+            if(nextIndex1 < 0 && nextIndex2 < 0) {
+                return true;
             }
-            else if(!stack.isEmpty()) {
-                stack.pop();
+            
+            if(nextIndex1 < 0 || nextIndex2 < 0) {
+                return false;
             }
+            
+            if(s.charAt(nextIndex1) != t.charAt(nextIndex2)) {
+                return false;
+            }
+            
+            index1 = nextIndex1 - 1;
+            index2 = nextIndex2 - 1;
         }
         
-        return String.valueOf(stack);
-    }*/
+        return true;
+    }
     
-    private String backspace(String s) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for(char c : s.toCharArray()) {
-            if(c != '#') {
-                stringBuilder.append(c);
+    private int getNextValidCharIndex(String s, int index) {
+        int backspaces = 0;
+        while(index >= 0) {
+            if(s.charAt(index) == '#') {
+                backspaces++;
             }
-            else if(stringBuilder.length() > 0) {
-                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            else if(backspaces > 0) {
+                backspaces--;
             }
+            else {
+                break;
+            }
+            
+            index--;
         }
         
-        return stringBuilder.toString();
-    }    
+        return index;
+    }
 }
