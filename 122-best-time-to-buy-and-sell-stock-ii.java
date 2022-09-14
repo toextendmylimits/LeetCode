d8b597a2474d4f1a4d4d89af4b1dce0038eec5bd
toextendmylimits <bruce.yang.it@gmail.com>
2022-09-14 22:24:51 +1000

    Update 122-best-time-to-buy-and-sell-stock-ii.java

--- 122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     // Greedy algorithm
+    // If stock price on a given day is higher than its previou day, always buy from its previous day, and sell on that day, to get profit
     public int maxProfit(int[] prices) {
         int accumulatedProfit = 0;
         for(int i = 1; i < prices.length; i++) {
