46304003acd0be1cb1b73ea4d5e907e523f8bda5
toextendmylimits <bruce.yang.it@gmail.com>
2022-09-22 19:06:45 +1000

    Update 20-valid-parentheses.java

--- 20-valid-parentheses.java
+++ 20-valid-parentheses.java
@@ -1,28 +1,28 @@
 class Solution {
-    private static final Map<Character, Character> bracketMap = new HashMap<>();
+    // For open bracket, push into stack
+    // For close bracket, if stack is empty, or popped element is not matched open bracket, return false
+    // In the end, it's valid if stack is empty
+    private static final Map<Character, Character> CLOSE_BRACKET_OPEN_BRACKET_MAP = new HashMap<>();
     static {
-        bracketMap.put(')','(');
-        bracketMap.put('}','{');
-        bracketMap.put(']','[');
+        CLOSE_BRACKET_OPEN_BRACKET_MAP.put(')', '(');
+        CLOSE_BRACKET_OPEN_BRACKET_MAP.put(']', '[');
+        CLOSE_BRACKET_OPEN_BRACKET_MAP.put('}', '{');
+        //To add more
     }
-    
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(stack.isEmpty()) {
-                stack.add(c);
+         Deque<Character> stack = new ArrayDeque<>();
+        for(int i  = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(CLOSE_BRACKET_OPEN_BRACKET_MAP.containsKey(ch)) {
+                if(stack.isEmpty() || stack.pop() != CLOSE_BRACKET_OPEN_BRACKET_MAP.get(ch)) {
+                    return false;
+                }
             }
             else {
-                if(stack.peek() == bracketMap.get(c)) {
-                    stack.pop();
-                }
-                else {
-                    stack.add(c);
-                }
+                stack.push(ch);
             }
         }
-        
+       
         return stack.isEmpty();
     }
 }
