d6e83cdb71962871d63d08e1a9ab49bdb4b33b76
toextendmylimits <bruce.yang.it@gmail.com>
2022-09-22 17:54:13 +1000

    Update 13-roman-to-integer.java

--- 13-roman-to-integer.java
+++ 13-roman-to-integer.java
@@ -1,25 +1,23 @@
 class Solution {
-    static final Map<Character, Integer> ROMAN_LETTER_VALUE_MAP = new HashMap<>();
+    private static final Map<Character, Integer> ROMAN_LETTER_VALUE = new HashMap<>();
     static {
-        ROMAN_LETTER_VALUE_MAP.put('I', 1);
-        ROMAN_LETTER_VALUE_MAP.put('V', 5);
-        ROMAN_LETTER_VALUE_MAP.put('X', 10);
-        ROMAN_LETTER_VALUE_MAP.put('L', 50);
-        ROMAN_LETTER_VALUE_MAP.put('C', 100);
-        ROMAN_LETTER_VALUE_MAP.put('D', 500);
-        ROMAN_LETTER_VALUE_MAP.put('M', 1000);
+        ROMAN_LETTER_VALUE.put('I', 1);
+        // To add more
+        ROMAN_LETTER_VALUE.put('V', 5);
+        ROMAN_LETTER_VALUE.put('X', 10);
+        ROMAN_LETTER_VALUE.put('L', 50);
+        ROMAN_LETTER_VALUE.put('C', 100);
+        ROMAN_LETTER_VALUE.put('D', 500);
+        ROMAN_LETTER_VALUE.put('M', 1000);
     }
     
     public int romanToInt(String s) {
-        int sum = 0;
-        for(int i = 0; i < s.length() - 1; i++) {
-            int current = ROMAN_LETTER_VALUE_MAP.get(s.charAt(i));
-            int next = ROMAN_LETTER_VALUE_MAP.get(s.charAt(i + 1));
-            
-            sum += current < next? -current : current;
+       int sum = 0;
+        for(int i = 0; i < s.length(); i++) {
+            int value = ROMAN_LETTER_VALUE.get(s.charAt(i));
+            boolean smallerThanNextDigit = i + 1 < s.length() && value < ROMAN_LETTER_VALUE.get(s.charAt(i + 1));
+            sum += smallerThanNextDigit? -value : value;
         }
-        
-        sum += ROMAN_LETTER_VALUE_MAP.get(s.charAt(s.length() - 1));
         
         return sum;
     }
