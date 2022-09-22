c6830db69ecef4a04e56d7ec52638f6b64cb4670
toextendmylimits <bruce.yang.it@gmail.com>
2022-09-22 11:21:46 +1000

    Update 7-reverse-integer.java

--- 7-reverse-integer.java
+++ 7-reverse-integer.java
@@ -1,22 +1,27 @@
 class Solution {
+    // Repeatedly pop the last digit of x, and push it to the back of the reversed number
     public int reverse(int x) {
-        long result = 0;
+        int result = 0;
         while(x != 0) {
-            int pop = x % 10;
-            /*if(result > Integer.MAX_VALUE / 10 || result == Integer.MAX_VALUE / 10  && pop > 7) {
-                return 0;
+            int remainder = x % 10;            
+            // Check overflow
+            if(result > 0) {
+                int tenthOfMax = Integer.MAX_VALUE / 10 ;
+                if(result > tenthOfMax|| (result == tenthOfMax && remainder > Integer.MAX_VALUE % 10)) {
+                    return 0;
+                }
+            }
+            else {
+                int tenthOfMin = Integer.MIN_VALUE / 10;
+                if(result < tenthOfMin || (result == tenthOfMin && remainder < Integer.MIN_VALUE % 10)) {
+                    return 0;
+                }
             }
             
-            if(result < Integer.MIN_VALUE / 10 || result == Integer.MIN_VALUE / 10 && pop < -8) {
-                return 0;
-            }*/
-            
-            result = result * 10 + pop;
+            result = result * 10 + remainder;
             x /= 10;
         }
         
-        if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) { return 0; }
-        
-        return (int)result;
+        return result;
     }
 }
