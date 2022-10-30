23317dc2dd7288a21b61e16a21a98d86e3dd8c0b
toextendmylimits <bruce.yang.it@gmail.com>
2022-10-31 10:12:23 +1100

    Update 162-find-peak-element.java

--- 162-find-peak-element.java
+++ 162-find-peak-element.java
@@ -1,17 +1,32 @@
+//Check whether nums[i] > nums[i + 1]
+//Three case:
+//1. All ascending
+//2. All descending
+//3. Ascend first and Descend later
 class Solution {
+    /*public int findPeakElement(int[] nums) {
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+        
+        return nums.length - 1;
+    }*/
+    
     public int findPeakElement(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
         while(left < right) {
-            int middle = left + (right - left) / 2;
-            if(nums[middle] > nums[middle + 1]) {
-                right = middle;
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > nums[mid + 1]) {
+                right = mid;
             }
             else {
-                left = middle + 1;
+                left = mid + 1;
             }
         }
         
-        return left;
-    }
+        return right;
+    }    
 }
