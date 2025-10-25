0891ab1f14ac122db2d386ee6172f49a6ab30968
toextendmylimits <bruce.yang.it@gmail.com>
2025-10-25 15:33:32 -0700

    Update 228_summary_range.py

--- top150/228_summary_range.py
+++ top150/228_summary_range.py
@@ -1,15 +1,16 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         result = []
-        start = 0
-        end = 0
-        while end < len(nums):
-            if end + 1 < len(nums) and nums[end + 1] == nums[end] + 1:
-                end += 1
-            else:
-                result.append(self.formatRange(nums[start], nums[end]))
-                start = end + 1
-                end = start
+ 
+        i = 0
+        while i < len(nums):
+            start = nums[i]
+            while i + 1 < len(nums) and nums[i + 1] == nums[i] + 1:
+                i += 1
+            
+            result.append(self.formatRange(start, nums[i]))
+            i += 1
+
         return result
     
     def formatRange(self, start, end):
