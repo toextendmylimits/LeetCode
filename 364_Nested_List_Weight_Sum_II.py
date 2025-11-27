3afbfd0ce6ab7e416e4d3d6aecb6f83d3d85c975
toextendmylimits <bruce.yang.it@gmail.com>
2025-11-27 14:23:38 -0800

    Update 364_Nested_List_Weight_Sum_II.py

--- 364_Nested_List_Weight_Sum_II.py
+++ 364_Nested_List_Weight_Sum_II.py
@@ -23,3 +23,26 @@ class Solution:
             total += runningSum
 
         return total
+
+# DFS
+class Solution:
+    def depthSumInverse(self, nested_list: List[NestedInteger]) -> int:
+        max_depth = self.calc_max_depth(nested_list, 1)
+        return self.calc_weight_sum(nested_list, 1, max_depth)
+    
+    def calc_max_depth(self, nested_list, depth):
+        max_depth = depth
+        for item in nested_list:
+            if not item.isInteger():
+                max_depth = max(max_depth, self.calc_max_depth(item.getList(), depth + 1))
+        return max_depth
+    
+    def calc_weight_sum(self, nested_list, depth, max_depth):
+        total = 0
+        for item in nested_list:
+            if item.isInteger():
+                weight = max_depth - depth + 1
+                total += weight * item.getInteger()
+            else:
+                total += self.calc_weight_sum(item.getList(), depth + 1, max_depth)
+        return total
