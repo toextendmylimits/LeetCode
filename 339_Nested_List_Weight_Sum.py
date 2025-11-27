b500e0e7924d0701a21bf2f90fb1f4e0bd86345c
toextendmylimits <bruce.yang.it@gmail.com>
2025-11-27 13:51:07 -0800

    Update 339_Nested_List_Weight_Sum.py

--- 339_Nested_List_Weight_Sum.py
+++ 339_Nested_List_Weight_Sum.py
@@ -12,3 +12,24 @@ class Solution:
             return total
         
         return dfs(nestedList, 1)
+
+# Approach 2: BFS
+from collections import deque
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        total = 0
+        queue = deque(nestedList)
+        depth = 1
+
+        while queue:
+            for _ in range(len(queue)):   # fixed level size loop
+                elem = queue.popleft()
+                if elem.isInteger():
+                    total += elem.getInteger() * depth
+                else:
+                    queue.extend(elem.getList())
+            depth += 1
+
+        return total
+
